2ac52ede237b76cc8bcae07a188a38328435dedb
Thomas R. Koll <tomk32@tomk32.de>
2011-04-30 17:01:52 +0200

    * Easier font management * Start selecting inventory rows

--- game/views/game_view.js
+++ game/views/game_view.js
@@ -1,13 +1,21 @@
 
 var GameView = function(target, game) {
   this.game = game;
-  this.canvas = new Canvas(target, 0, function() { 
-    this.clear();
-    this.fillColor('#eee')
-    this.fillRect(0, 0, this.width, this.height)
-    this.fillColor('#000')
-    this.fillText('v' + Game.version, 0, this.height-20);
-  });
+  this.canvas = new Canvas(target, 0, this.redraw);
+  
+  this.canvas.onMousedown = function(x,y,button) { console.log(x);};
 
   this.inventory_view = new InventoryView(this.canvas, this.game.inventory);
 };
+
+GameView.prototype.redraw = function() {
+  this.clear();
+  this.font_regular();
+  this.fillColor('#eee')
+  this.fillRect(0, 0, this.width, this.height)
+  this.fillColor('#000')
+  this.fillText('v' + Game.version, 0, this.height-20);
+};
+
+Canvas.prototype.font_regular = function() { this.font('400 13px sans-serif'); }
+Canvas.prototype.font_strong  = function() { this.font('800 13px sans-serif'); }
--- game/views/inventory_view.js
+++ game/views/inventory_view.js
@@ -1,27 +1,49 @@
 var InventoryView = function(canvas, inventory) {
   this.inventory = inventory;
-  this.canvas = canvas.createLayerAt('inventory', 20, -160, 400, canvas.height-200, 1, this.redraw, 4, true);
+  this.canvas = canvas.createLayerAt('inventory', 20, -160, 400, canvas.height-200, 10, this.redraw, 4, true);
   this.canvas.parent = this;
-  this.canvas.onMousedown = function(x,y,button) { console.log(x);};
-  console.log(this.canvas.onMousedown);
+  this.canvas.onMouseDown = this.mouseDown;
+  this.canvas.onMouseUp = this.mouseUp;
+  this.rowHeight = 20;
+  this.selected = false;
 }
 
 InventoryView.prototype.redraw = function(frameDuration, totalDuration, frameNumber) {
   this.clear();
+  this.font_regular();
   this.fillColor('#fff')
   this.fillRect(0, 0, this.width, this.height)
   this.fillColor('#000')
-  
-  this.fillText('Inventory', 5, 5);
-  var pos_y = 30;
+
+  var pos_y = 0;
   if(this.parent) {
     for(var c in this.parent.inventory.items) {
+      this.font_regular();
+      if(c == this.parent.selectedRow()) {
+        this.font_strong();
+      }
       this.fillText(this.parent.inventory.items[c].to_s(), 20, pos_y);
-      pos_y += 20;
+      pos_y += this.parent.rowHeight;
     }
   }
+};
+
+InventoryView.prototype.selectRow = function(row) {
+  if(this.selected) { return true; }
+  var item = this.inventory.items[row];
+  if(!item) { return false; }
+  this.selected = true;
+  // new selection
 }
 
+// find which row the mouse is over
+InventoryView.prototype.selectedRow = function() {
+  return Math.floor(this.canvas.mouse.y / this.rowHeight);
+};
 InventoryView.prototype.mouseDown = function(x,y,button) {
-  console.log(x);
+  this.parent.selectRow(this.parent.selectedRow(this.parent.canvas.mouse));
+};
+
+InventoryView.prototype.mouseUp = function(x,y,button) {
+  this.parent.selected = false;
 }
