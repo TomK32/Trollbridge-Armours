18e49f36ef9ab8cda49a0657ff15d24fce728ec6
Thomas R. Koll <tomk32@tomk32.de>
2011-05-01 12:19:10 +0200

    Fix Hero spec

--- spec/javascripts/hero_spec.js
+++ spec/javascripts/hero_spec.js
@@ -1,6 +1,6 @@
 describe("Hero", function() {
   beforeEach(function() {
-    hero = new Hero('Thomas the Wellfed');
+    hero = new Hero({name: 'Thomas the Wellfed'});
   });
 
   it("should have name", function() {
@@ -8,7 +8,7 @@ describe("Hero", function() {
   });
   it("should have empty inventory", function() {
     expect(hero.inventory).toBeDefined();
-    expect(new Hero('Earth', [42]).inventory.items[0]).toEqual(42);
+    expect(new Hero({name: 'Earth', items: [42]}).inventory.items[0]).toEqual(42);
   });
   it("should have image", function() {
     expect(hero.image).toEqual('Thomas the Wellfed.png');
