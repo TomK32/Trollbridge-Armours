ecc315fdc719e96a91ec8551cf4c264e9d826e6f
Thomas R. Koll <tomk32@tomk32.de>
2011-05-07 10:38:26 +0200

    Own data for specs

---
+++ spec/javascripts/helpers/data.js
@@ -0,0 +1,107 @@
+var allRecipes, key, _i, _j, _k, _l, _len, _len2, _len3, _len4, _ref, _ref2, _ref3, _ref4;
+Ingredient.allIngredients = {
+  oak_wood: {
+    name: 'Oak Wood',
+    category: 'Wood',
+    value: 1
+  },
+  oak_plank: {
+    name: 'Oak Plank',
+    category: 'Plank',
+    value: 2
+  },
+  iron: {
+    name: 'Iron',
+    category: 'Metal',
+    value: 4
+  },
+  leather: {
+    name: 'Leather',
+    category: 'Leather',
+    value: 2
+  },
+  iron_ore: {
+    name: 'Iron Ore',
+    category: 'Ore',
+    value: 3
+  }
+};
+_ref = Object.keys(Ingredient.allIngredients);
+for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+  key = _ref[_i];
+  Ingredient.define(key, Ingredient.allIngredients[key]);
+}
+Product.allProducts = {
+  wooden_sword: {
+    name: 'Wooden Sword',
+    category: 'Sword',
+    value: 5
+  },
+  wooden_shield: {
+    name: 'Wooden Shield',
+    category: 'Shield',
+    value: 5
+  },
+  iron_sword: {
+    name: 'Iron Sword',
+    category: 'Sword',
+    value: 10
+  }
+};
+_ref2 = Object.keys(Product.allProducts);
+for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+  key = _ref2[_j];
+  Product.define(key, Product.allProducts[key]);
+}
+allRecipes = {
+  oak_plank: {
+    ingredients: [Ingredient.oak_wood(3)],
+    products: [Ingredient.oak_plank(1)]
+  },
+  wooden_sword: {
+    ingredients: [Ingredient.oak_plank(2), Ingredient.leather(1)],
+    products: [Product.wooden_sword(1)]
+  },
+  wooden_shield: {
+    ingredients: [Ingredient.oak_plank(2)],
+    products: [Product.wooden_shield(1)],
+    requirements: [['Recipe.oak_plank', 2]]
+  },
+  iron: {
+    ingredients: [Ingredient.iron_ore(4)],
+    products: [Ingredient.iron(1)]
+  },
+  iron_sword: {
+    ingredients: [Ingredient.iron(3), Ingredient.leather(1)],
+    products: [Product.iron_sword(1)],
+    requirements: [['Recipe.iron', 3]]
+  }
+};
+_ref3 = Object.keys(allRecipes);
+for (_k = 0, _len3 = _ref3.length; _k < _len3; _k++) {
+  key = _ref3[_k];
+  Recipe.define(key, allRecipes[key]);
+}
+Hero.allHeroes = {
+  wendy: {
+    name: 'Windy Wendy',
+    items: [Product.iron_sword(1), Product.wooden_shield(1)]
+  },
+  cagua: {
+    name: 'Cagua',
+    items: []
+  },
+  riel: {
+    name: 'Louis David Riel',
+    items: [Product.wooden_sword(1)]
+  },
+  picasse: {
+    name: 'Picasse',
+    items: [Product.wooden_sword(1)]
+  }
+};
+_ref4 = Object.keys(Hero.allHeroes);
+for (_l = 0, _len4 = _ref4.length; _l < _len4; _l++) {
+  key = _ref4[_l];
+  Hero.define(key, Hero.allHeroes[key]);
+}
